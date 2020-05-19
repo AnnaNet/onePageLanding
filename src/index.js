ace876b10003a7de6efb08384b01cbb9f7215a2b
Anna Moskalchuk <anna@crystax.net>
2020-05-19 18:42:12 +0300

    feat: add logo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import $ from 'jquery';
 import 'bootstrap';
 
-$(document).ready(function temp() {
-  let el=document.getElementById("head");
+$(document).ready(function addMainPict() {
+  let mainPict = document.getElementById("head");
+  mainPict.style.backgroundImage = "url('./dist/images/head.png')";
 
-  console.log ("I'm element id=head --> " + el);
-
-  //el.style.background = 'red';
-  el.style.backgroundImage = "url('./dist/images/head.png')";
+  let logo = document.getElementById("logo");
+  logo.style.backgroundImage = "url('./dist/images/logo.png')";
 });
